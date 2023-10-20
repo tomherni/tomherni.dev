e8c8521a8ff9616db93309251b5dfcf2bcd6759e
Tom Herni <tomherni@gmail.com>
2023-10-20 23:07:11 +0200

    Minify HTML and CSS in build step

--- build.mjs
+++ build.mjs
@@ -1,5 +1,8 @@
 import fs from 'fs';
+import path from 'path';
 import { copySync } from 'fs-extra/esm';
+import CleanCSS from 'clean-css';
+import { minify } from 'html-minifier-terser';
 
 const SRC_PATH = 'src';
 const DIST_PATH = 'dist';
@@ -11,3 +14,14 @@ fs.mkdirSync(DIST_PATH);
 // Copy contents of SRC to DIST.
 copySync(SRC_PATH, DIST_PATH);
 
+// Minify CSS.
+const cssPath = path.join(DIST_PATH, 'css/styles.css')
+const unminifiedStyles = fs.readFileSync(cssPath, 'utf8');
+const { styles } = new CleanCSS().minify(unminifiedStyles);
+fs.writeFileSync(cssPath, styles);
+
+// Minify HTML.
+const htmlPath = path.join(DIST_PATH, 'index.html');
+const unminifiedHtml = fs.readFileSync(htmlPath, 'utf8');
+const html = await minify(unminifiedHtml, { collapseWhitespace: true });
+fs.writeFileSync(htmlPath, html);
