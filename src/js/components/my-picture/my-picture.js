5847b481f9f49aa5bdaa6a90d57fe8b9c21cfe7c
Tom Herni <tomherni@gmail.com>
2023-10-29 21:34:06 +0100

    Add a my-picture component

---
+++ src/js/components/my-picture/my-picture.js
@@ -0,0 +1,26 @@
+import { renderStylesToShadowRoot } from '../../utils/styles.js';
+import { styles } from './styles.js';
+
+export class MyPicture extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+
+    this.shadowRoot.innerHTML = `
+      <div class="🐸">
+        <picture>
+          <source srcset="./img/picture.webp" type="image/webp" />
+          <img
+            src="./img/picture.png"
+            width="150"
+            height="150"
+            alt="Picture of Tom sitting outside and smiling. He is happy you're here!"
+          />
+        </picture>
+      </div>
+    `;
+
+    renderStylesToShadowRoot(this.shadowRoot, styles);
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import { CtaLink } from './components/cta-link/cta-link.js';
+import { MyPicture } from './components/my-picture/my-picture.js';
 import { ThemeSwitch } from './components/theme-switch/theme-switch.js';
 
 customElements.define('cta-link', CtaLink);
+customElements.define('my-picture', MyPicture);
 customElements.define('theme-switch', ThemeSwitch);
