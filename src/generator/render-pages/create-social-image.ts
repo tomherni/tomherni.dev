c50b263907464c7a8abdad5811e467cb7d18deb2
Tom <tomherni@gmail.com>
2025-01-09 22:10:16 +0100

    feat: use node-canvas instead of Puppeteer for creating social images (#11)

--- src/generator/render-pages/create-social-image.ts
+++ src/generator/render-pages/create-social-image.ts
@@ -1,54 +1,83 @@
+import fs from 'fs';
 import path from 'path';
 import url from 'url';
-import puppeteer from 'puppeteer';
+import {
+  CanvasRenderingContext2D,
+  createCanvas,
+  loadImage,
+  registerFont,
+} from 'canvas';
 import { Post } from '../../../types/types';
-import { SOCIAL_FILE_NAME } from '../constants';
+import { SOCIAL_FILE_NAME, SOCIAL_MIME_TYPE } from '../constants';
 
-export async function createSocialImage(
-  post: Post,
-  postFile: string,
-): Promise<void> {
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1200, height: 600 });
-  await page.goto(getTemplateFileUrl(), { waitUntil: 'networkidle0' });
+const WIDTH = 1200;
+const HEIGHT = 600;
+const PADDING = 100;
 
-  const template = await page.$('.card');
-  if (!template) {
-    throw new Error('Card elements not found');
+let fontsRegistered = false;
+
+export async function createSocialImage(post: Post, postFile: string) {
+  registerFonts();
+
+  const canvas = createCanvas(WIDTH, HEIGHT);
+  const ctx = canvas.getContext('2d');
+
+  // Create background and top border.
+  ctx.fillStyle = '#ffffff';
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  ctx.fillStyle = '#E8422C';
+  ctx.fillRect(0, 0, WIDTH, 24);
+
+  // Write "tomherni.dev".
+  ctx.fillStyle = '#272626';
+  ctx.font = '44px "Source Sans 3 Bold"';
+  ctx.fillText('tomherni', PADDING, 114);
+  ctx.fillStyle = '#E8422C';
+  ctx.font = '44px "Source Sans 3 Bold"';
+  ctx.fillText('.dev', PADDING + ctx.measureText('tomherni').width, 114);
+
+  // Write the title that wraps when necessary.
+  ctx.fillStyle = '#272626';
+  ctx.font = '72px "Source Sans 3 Bold"';
+  writeWrappingTitle(post.meta.title, ctx);
+
+  // Insert the profile picture.
+  const dirName = url.fileURLToPath(new URL('.', import.meta.url));
+  const pictureUri = 'resources-social-image/picture.png';
+  const image = await loadImage(path.join(dirName, pictureUri));
+  ctx.drawImage(image, 948, 348, 180, 180);
+
+  // Save the social image.
+  const buffer = canvas.toBuffer(SOCIAL_MIME_TYPE);
+  const foo = path.join(path.dirname(postFile), SOCIAL_FILE_NAME);
+  fs.writeFileSync(foo, buffer);
+}
+
+function registerFonts() {
+  if (!fontsRegistered) {
+    fontsRegistered = true;
+    const dirName = url.fileURLToPath(new URL('.', import.meta.url));
+    const fontUri = 'resources-social-image/source-sans-3-bold.ttf';
+    registerFont(path.join(dirName, fontUri), { family: 'Source Sans 3 Bold' });
+  }
+}
+
+function writeWrappingTitle(title: string, ctx: CanvasRenderingContext2D) {
+  const words = title.split(' ');
+  let line = '';
+  let offsetTop = 225;
+
+  for (const word of words) {
+    const lineWithLatestWord = line + ' ' + word;
+
+    if (ctx.measureText(lineWithLatestWord).width > WIDTH - PADDING) {
+      ctx.fillText(line.trim(), PADDING, offsetTop);
+      line = word;
+      offsetTop = offsetTop + 80;
+    } else {
+      line = lineWithLatestWord;
+    }
   }
 
-  // TODO: title: less padding-right, and perhaps smaller line-height.
-  // TODO: tags: be careful not to overlap the image?
-  await page.evaluate(({ title, tags }) => {
-    const titleElement = document.querySelector('div.title') as HTMLElement;
-    titleElement.innerText = title;
-
-    const tagsElement = document.querySelector('ul.tags') as HTMLElement;
-    if (tags) {
-      tags.forEach((tag) => {
-        const li = document.createElement('li');
-        li.innerText = tag;
-        tagsElement.append(li);
-      });
-    } else {
-      tagsElement.remove();
-    }
-  }, post.meta);
-
-  await template.screenshot({ path: getSaveFilePath(postFile) });
-
-  await browser.close();
-}
-
-function getTemplateFileUrl(): string {
-  const dirName = url.fileURLToPath(new URL('.', import.meta.url));
-  const templatePath = path.join(dirName, 'template/template.html');
-  return url.pathToFileURL(templatePath).href;
-}
-
-function getSaveFilePath(postFile: string): string {
-  return path.join(path.dirname(postFile), SOCIAL_FILE_NAME);
+  ctx.fillText(line.trim(), PADDING, offsetTop);
 }
