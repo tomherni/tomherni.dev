a67127fdd514f5d5130832e1d51cc36556041045
Tom Herni <tomherni@gmail.com>
2025-01-12 22:31:44 +0100

    chore: update prettier and stylelint

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  singleQuote: true,
+  overrides: [
+    {
+      files: '*.html',
+      options: {
+        printWidth: 120,
+      },
+    },
+  ],
+};
+
+export default config;
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,13 @@
-module.exports = {
+/**
+ * @see https://stylelint.io/user-guide/configure/
+ * @type {import('stylelint').Config}
+ */
+const config = {
   extends: 'stylelint-config-standard',
   rules: {
     'media-feature-range-notation': null, // Lacks desired browser support
     'no-descending-specificity': null,
   },
 };
+
+export default config;
