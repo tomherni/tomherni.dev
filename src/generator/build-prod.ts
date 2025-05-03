761fdb06deeeee6b9db499691ee9dc48933c57c7
Tom <tomherni@gmail.com>
2025-05-03 21:47:24 +0200

    chore: test envs in Netlify builds

--- src/generator/build-prod.ts
+++ src/generator/build-prod.ts
@@ -1,3 +1,10 @@
 import { build } from './build';
 
+// Test: verifying envs in Netlify deploy preview and prod builds 
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('CONTEXT:', process.env.CONTEXT);
+console.log('DEPLOY_PRIME_URL:', process.env.DEPLOY_PRIME_URL);
+console.log('REVIEW_ID:', process.env.REVIEW_ID);
+console.log('URL:', process.env.URL);
+
 await build({ env: 'PROD', baseUrl: 'https://tomherni.dev/' });
