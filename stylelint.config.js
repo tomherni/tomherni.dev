7cda953f1aee986b199a1415dc7f6e6bd6fa4198
Tom Herni <tomherni@gmail.com>
2026-06-22 11:55:20 +0200

    feat: use new syntax for range media queries

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,7 +5,6 @@
 const config = {
   extends: 'stylelint-config-standard',
   rules: {
-    'media-feature-range-notation': null, // Lacks desired browser support
     'no-descending-specificity': null,
   },
 };
